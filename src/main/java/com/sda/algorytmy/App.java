6e7a943ad544da6bafba9a1f421c58c2e10cb860
Magdalena Jankowska/Magda <magda.desirec@gmail.com>
2019-05-12 15:50:39 +0100

    sredniaWynik

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,5 +1,7 @@
 package com.sda.algorytmy;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+            int wynik = 0;
+        for (int i = 0; i < N; i++){
+            int a = scanner.nextInt();
+
+            wynik = wynik +a;
+
+        }
+        wynik = wynik / N;
+        System.out.println(wynik);
+
+
+
     }
+
 }
